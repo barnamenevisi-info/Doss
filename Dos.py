7d814e1e293d7e53c5112c05339f35836185ee60
Mr-amir84 <86835225+Mr-amir84@users.noreply.github.com>
2021-07-02 19:36:37 +0430

    Add files via upload

---
+++ Dos.py
@@ -0,0 +1,44 @@
+import os
+os.system("pip install requests socket webbrowser random datetime urllib.request && pkg install sl -y && pkg install figlet -y && pkg install ruby -y && pkg install curl -y && gem install lolcat && pkg install toilet -y")
+import random
+import socket
+import webbrowser  
+import urllib.request                                         
+import random    
+import requests
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 80
+def back():
+    #os.system("sl")
+    os.system("clear")
+    os.system("toilet -f mono12 -F gay DDOS")
+def start():
+    global k
+    print("""  
+#############################################
+                                                                                     
+telegram channel : t.me/mramir840                   
+coded by : t.me/Theres_no_such_thing_as_security   
+                                                                                                                                                                                                                          
+               d88P8
+              d88P888  
+            d88P  888   8888b   d8888 
+           d88P   888   88888b.d88888 
+          d88P    888   888Y88888P888 
+         d88Pd88P8888   888 Y888P 888 
+        d88P      888   888  Y8P  888 
+       d88P       888   888   "   888 
+      d88P        888   888       888 
+
+
+#############################################                                                  
+
+[1]Attack with list proxy                                                                   
+""")
+    k = int(input("[#]Attack {1} ----->>> "))
+back()
+start()
+if k == 1:
+   import p.py
+
+            
